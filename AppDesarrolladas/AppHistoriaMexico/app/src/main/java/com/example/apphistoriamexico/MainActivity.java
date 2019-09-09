442b15249dfdd57cd353b663138618f45636e426
LeoSan <cuenca623@gmail.com>
2019-09-09 00:09:05 -0500

    Autor: Leonard Cuenca Fase  1 : Desarrollo y Diseño app de Guia para estudiar la hisotria de Mexico

---
+++ AppDesarrolladas/AppHistoriaMexico/app/src/main/java/com/example/apphistoriamexico/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.apphistoriamexico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    //Apunta al Activity de Guia (Cuestionario)
+    public void vistaGuia (View view){
+        Intent siguiente = new Intent(this,MainGuia.class);
+        startActivity(siguiente);
+    }
+
+    //Apunta al Activity de Preguntas (Aqui se realizaran las preguntas)
+    public void vistaPreguntas (View view){
+        Intent siguiente = new Intent(this,MainGuia.class);
+        startActivity(siguiente);
+    }
+
+
+}
