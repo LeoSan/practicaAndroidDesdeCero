0aed0166cb16d4f99133052245fbb182924b9e14
LeoSan <cuenca623@gmail.com>
2019-09-30 01:14:15 -0500

    Autor: Leonard Cuenca Se logrò crear botones dinamicos con bases de datos. Se establecio los  botones de las categorias dinamicamente. Se  logro anexar el evento onclick a los botones.

--- AppDesarrolladas/AppHistoriaMexico/app/src/main/java/com/example/apphistoriamexico/AdmiSQLiteOpenHelper.java
+++ AppDesarrolladas/AppHistoriaMexico/app/src/main/java/com/example/apphistoriamexico/AdmiSQLiteOpenHelper.java
@@ -18,23 +18,23 @@ public class AdmiSQLiteOpenHelper extends SQLiteOpenHelper {
         BasesDeDatos.execSQL("INSERT INTO t_categoria(id, nom_categoria, desp_categoria, activo) VALUES (1, 'Curiosidades ', 'Curiosidades ', 1), (2, 'Comida', 'Comida', 1),(3, 'Cine', 'cine', 1),(4, 'Musica / Arte', 'Musica / Arte', 1),(5, 'Deporte', 'Deporte', 1),(6, 'Prehispánica', 'Prehispánica', 1),(7, 'Nueva España', 'Nueva España', 1),(8, 'Independencia ', 'Independencia ', 1),(9, 'Revolución', 'Revolución', 1),(10, 'Historia Comtemponranea', 'Historia Comtemponranea', 1),(11, 'Geografía', 'Geografía', 1),(12, 'Legislación Política ', 'Legislación Política ', 1)");
 
 // tabla de complemento
-        BasesDeDatos.execSQL("Create table t_complemento(id int primary Key, co_pregunta int, complemento text, activo int)");
+//        BasesDeDatos.execSQL("Create table t_complemento(id int primary Key, co_pregunta int, complemento text, activo int)");
 
 
-        BasesDeDatos.execSQL("Create table t_estadisticas(id int primary Key, co_estudios int, co_pregunta int, aprobada int)");
-        BasesDeDatos.execSQL("Create table t_estudios(id int primary Key, fecha date, co_actividad int)");
-        BasesDeDatos.execSQL("Create table t_preguntas(id int primary Key, co_categoria int, co_respuesta int, pregunta text, respuesta text, enlace_resp text, imagen_pre text, imagen_resp text, activo int)");
+//        BasesDeDatos.execSQL("Create table t_estadisticas(id int primary Key, co_estudios int, co_pregunta int, aprobada int)");
+//        BasesDeDatos.execSQL("Create table t_estudios(id int primary Key, fecha date, co_actividad int)");
+//        BasesDeDatos.execSQL("Create table t_preguntas(id int primary Key, co_categoria int, co_respuesta int, pregunta text, respuesta text, enlace_resp text, imagen_pre text, imagen_resp text, activo int)");
 
     // Tabla Arbol de repasos
-        BasesDeDatos.execSQL("Create table t_repaso_arbol(id int primary Key, co_categoria int, nom_repaso text, icono text, activo int)");
-        BasesDeDatos.execSQL("INSERT INTO t_repaso_arbol(id, co_categoria, nom_repaso, icono, activa) VALUES (1, 1, 'Básico', '0', 1), (2, 1, 'Medio', '0', 1), (3, 1, 'Avanzado', '0', 1), (4, 1, 'Experto', '0', 1), (5, 1, 'Leyenda', '0', 1)");
+//        BasesDeDatos.execSQL("Create table t_repaso_arbol(id int primary Key, co_categoria int, nom_repaso text, icono text, activo int)");
+//        BasesDeDatos.execSQL("INSERT INTO t_repaso_arbol(id, co_categoria, nom_repaso, icono, activa) VALUES (1, 1, 'Básico', '0', 1), (2, 1, 'Medio', '0', 1), (3, 1, 'Avanzado', '0', 1), (4, 1, 'Experto', '0', 1), (5, 1, 'Leyenda', '0', 1)");
 
 
-        BasesDeDatos.execSQL("Create table t_repaso_preguntas(id int primary Key, co_repaso_arbol int, co_preguntas int, aprendida int, leida int, activo int)");
+//       BasesDeDatos.execSQL("Create table t_repaso_preguntas(id int primary Key, co_repaso_arbol int, co_preguntas int, aprendida int, leida int, activo int)");
 
     // Tabla tipo Actividad
-        BasesDeDatos.execSQL("Create table t_tipo_actividad(id int primary Key, nom_actividad text, icono text, activo int)");
-        BasesDeDatos.execSQL("INSERT INTO t_tipo_actividad(id, nom_actividad, icono, activo ) VALUES (1, 'Cuestionario', '', 1), (2, 'Reta ', '', 1)");
+//        BasesDeDatos.execSQL("Create table t_tipo_actividad(id int primary Key, nom_actividad text, icono text, activo int)");
+//        BasesDeDatos.execSQL("INSERT INTO t_tipo_actividad(id, nom_actividad, icono, activo ) VALUES (1, 'Cuestionario', '', 1), (2, 'Reta ', '', 1)");
 
 
 
--- AppDesarrolladas/AppHistoriaMexico/app/src/main/java/com/example/apphistoriamexico/MainGuia.java
+++ AppDesarrolladas/AppHistoriaMexico/app/src/main/java/com/example/apphistoriamexico/MainGuia.java
@@ -2,13 +2,93 @@ package com.example.apphistoriamexico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//Librerias para la BD
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 
 public class MainGuia extends AppCompatActivity {
+
+    //Declaracion de Variables
+    LinearLayout contBtnCategorias;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_guia);
+
+        contBtnCategorias = (LinearLayout)findViewById(R.id.contBtnCategorias);
+
+        //Obtener los valores de categoria
+        agregarBotones(getCategorias(), contBtnCategorias);
+
     }
+
+
+    //Metodos interfaz
+
+    //Este Metodo permite crear los botones de las categorias
+    public void agregarBotones(Cursor datosCategoria, LinearLayout contenedor ){
+
+        if ( datosCategoria.moveToFirst() ) {//Muestra los valores encontrados en la consulta
+
+            do{
+
+                //Instancio el boton
+                Button botonCategoria = new Button( getApplicationContext() );
+
+                //Le coloco nombre al boton
+                botonCategoria.setText(datosCategoria.getString(1)); //aqui Obtenemos el nom_categoria Indice 1
+
+                // Forma para anexar un evento al boton
+                botonCategoria.setOnClickListener(interfazEstdudio);
+
+                // Forma para anexar el boton al contenedor
+                contenedor.addView(botonCategoria);
+
+            }while( datosCategoria.moveToNext() );
+
+        }else{
+            Toast.makeText(this, "Error, Falla en la  Bases de datos en la tabla categorias. ", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+    //Eventos
+
+    private View.OnClickListener interfazEstdudio = new View.OnClickListener(){
+        public void onClick(View v){
+            Button objBoton = (Button) v;
+            Toast.makeText(getApplicationContext(), "Se preciono el " + objBoton.getText(), Toast.LENGTH_LONG ).show();
+
+        }
+    };
+
+
+
+
+
+
+
+    // Metodos Conexion Bases de Datos
+
+
+    //Metodo Devuelve un cursos con los valores de la categoria
+    public Cursor getCategorias(){
+        //Consulta Categoria
+        AdmiSQLiteOpenHelper admin = new AdmiSQLiteOpenHelper(this, "administracion", null, 1 );
+        // Abre la base de datos en modo lectura y escritura
+        SQLiteDatabase BasesDeDatos = admin.getWritableDatabase();
+        Cursor DatosCategoria = BasesDeDatos.rawQuery("SELECT id, nom_categoria, desp_categoria FROM t_categoria WHERE activo = 1 ORDER BY id ASC", null);
+        return DatosCategoria;
+    }
+
 }
