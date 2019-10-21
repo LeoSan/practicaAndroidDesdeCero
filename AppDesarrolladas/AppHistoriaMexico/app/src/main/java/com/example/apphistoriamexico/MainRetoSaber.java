15fae77e9b84d8b91c670b060d0650e340e392a6
dgticexterno.45 <ljcuenca@indracompany.com>
2019-10-21 15:31:43 -0500

    Autor: Leonard Cuenca Descripción: Desarrollo de la intrefaz de 2.3, nos dimos cuenta que al desplegar no actualiza la clase SQLlite, porque ya realice los cambios de los insert y me esta mostrando los insert anteriores.

--- AppDesarrolladas/AppHistoriaMexico/app/src/main/java/com/example/apphistoriamexico/MainRetoSaber.java
+++ AppDesarrolladas/AppHistoriaMexico/app/src/main/java/com/example/apphistoriamexico/MainRetoSaber.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 public class MainRetoSaber extends AppCompatActivity {
 
     TextView labelNivel, labelCategoria;
-    Cursor categoriaId, nivelesId, preguntaId, listPreguntas;
+    Cursor categoriaId, nivelesId,  listPreguntas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +33,13 @@ public class MainRetoSaber extends AppCompatActivity {
         String IdNivel     = getIntent().getStringExtra("IdNivel");
         String Contador    = getIntent().getStringExtra("IdContador");
 
-        Toast.makeText(getApplicationContext(), "Nivel " + IdNivel + " | Contador -> " + Contador , Toast.LENGTH_LONG ).show();
-
         //Elementos Dinamicos.
         setNomNiveles(IdNivel, Contador, IdCategoria);
         setNomCategoria(IdCategoria);
+
+        // practica
+          getPreguntasNivelCategoria(IdCategoria, IdNivel);
+
     }
 
     //Redirect-> Redirecciona a la interfaz de Apoyo de memoria
@@ -88,19 +90,50 @@ public class MainRetoSaber extends AppCompatActivity {
         AdmiSQLiteOpenHelper admin = new AdmiSQLiteOpenHelper(this, "administracion", null, 1 );
         // Abre la base de datos en modo lectura y escritura
         SQLiteDatabase BasesDeDatos = admin.getWritableDatabase();
-        Cursor consultaIdPreguntas = BasesDeDatos.rawQuery("SELECT id FROM t_preguntas WHERE co_categoria = 1 AND co_nivel = 1 AND activo = 1 ", null);
+        Cursor consultaIdPreguntas = BasesDeDatos.rawQuery("SELECT count(id) total FROM t_preguntas WHERE co_categoria = "+idCategoria+" AND co_nivel = " +idNivel, null);
+        //
         // BasesDeDatos.close();
         return consultaIdPreguntas;
     }
 
     //Metodo Devuelve un cursor con la Pregunta Unica
-    public Cursor getPreguntaId(String id){
+    public Cursor getPreguntasNivelCategoria(String idCategoria, String idNivel){
         //Consulta Los niveles de aprendizaje
         AdmiSQLiteOpenHelper admin = new AdmiSQLiteOpenHelper(this, "administracion", null, 1 );
         // Abre la base de datos en modo lectura y escritura
         SQLiteDatabase BasesDeDatos = admin.getWritableDatabase();
-        Cursor consultaId = BasesDeDatos.rawQuery("SELECT id, co_respuesta, co_categoria, co_nivel, pregunta, respuesta FROM t_preguntas WHERE id = " + id + " AND activo = 1 ", null);
+        Cursor consultaId = BasesDeDatos.rawQuery("SELECT id, co_respuesta, co_categoria, co_nivel, pregunta, respuesta FROM t_preguntas WHERE co_categoria = 1 AND co_nivel =  1" , null);
         // BasesDeDatos.close();
+
+        try {
+
+            if (consultaId != null) {
+                consultaId.moveToFirst();
+                do {
+                    //Asignamos el valor en nuestras variables para usarlos en lo que necesitemos
+                    String idPregunta = consultaId.getString(consultaId.getColumnIndex("id"));
+                    String sPregunta = consultaId.getString(consultaId.getColumnIndex("pregunta"));
+
+
+                       System.out.println( "ID = " + consultaId.getString(0) );
+                       System.out.println( "pregunta = " + consultaId.getString(consultaId.getColumnIndex("pregunta")) );
+                       System.out.println();
+
+                } while (consultaId.moveToNext());
+            }else{
+                System.out.println( " -------------  No hay datos ------------------"  );
+                System.out.println();
+            }
+
+        } catch ( Exception e ) {
+              System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+              System.exit(0);
+        }
+
+        //Cerramos el cursor y la conexion con la base de datos
+        consultaId.close();
+        BasesDeDatos.close();
+
         return consultaId;
     }
 
@@ -112,8 +145,14 @@ public class MainRetoSaber extends AppCompatActivity {
     public void setNomNiveles (String IdNivel, String  Contador, String IdCategoria){
 
         nivelesId = getNivelId( IdNivel );
+        Cursor  preguntaId =  getPreguntas(IdCategoria, IdNivel );
+
+        if (preguntaId.moveToFirst()){
+            System.out.println( "El total de registros es : " + preguntaId.getString(0));
+        }
+
         if ( nivelesId.moveToFirst() ){//Muestra los valores encontrados en la consulta
-            labelNivel.setText( nivelesId.getString(1) + " - Preguntas : "+Contador+" /  19 " );
+            labelNivel.setText( nivelesId.getString(1) + " - Preguntas : " + Contador + " /   " + preguntaId.getString(0) );
         }
 
     }
@@ -125,5 +164,7 @@ public class MainRetoSaber extends AppCompatActivity {
             labelCategoria.setText( categoriaId.getString(1) );
         }
     }
+
+
 
 }// Fin de la clase
